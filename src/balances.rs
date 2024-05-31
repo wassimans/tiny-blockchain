55f3d3b9bb1758ac05ad626ee9519bfd3f974a79
Wassim Mansouri <wassim.mansouri@gmail.com>
2024-05-31 21:38:17 +0200

    Add num crate Make Balances pallet generic

--- src/balances.rs
+++ src/balances.rs
@@ -1,78 +1,87 @@
-
+use num::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
 /// This is the Balances Module.
 /// It is a simple module which keeps track of how much balance each account has in this state
 /// machine.
-pub struct Pallet {
-    // A simple storage mapping from accounts (`String`) to their balances (`u128`).
-    balances: BTreeMap<String, u128>,
+#[derive(Debug)]
+pub struct Pallet<AccountId, Balance> {
+	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
+	balances: BTreeMap<AccountId, Balance>,
 }
 
-impl Pallet {
-    /// Create a new instance of the balances module.
-    pub fn new() -> Self {
-        Self {
-            balances: BTreeMap::new()
-        }
-    }
+impl<AccountId, Balance> Pallet<AccountId, Balance>
+where
+	AccountId: Ord,
+	Balance: Zero + CheckedSub + CheckedAdd + Copy,
+{
+	/// Create a new instance of the balances module.
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
 
-    /// Set the balance of an account `who` to some `amount`.
-    pub fn set_balance(&mut self, who: &String, amount: u128) {
-        self.balances.insert(who.clone(), amount);
-    }
+	/// Set the balance of an account `who` to some `amount`.
+	pub fn set_balance(&mut self, who: AccountId, amount: Balance) {
+		self.balances.insert(who, amount);
+	}
 
-    /// Get the balance of an account `who`.
-    /// If the account has no stored balance, we return zero.
-    pub fn balance(&self, who: &String) -> u128 {
-        *self.balances.get(who).unwrap_or(&0)
-    }
+	/// Get the balance of an account `who`.
+	/// If the account has no stored balance, we return zero.
+	pub fn balance(&self, who: &AccountId) -> Balance {
+		*self.balances.get(who).unwrap_or(&Zero::zero())
+	}
 
-    /// Transfer `amount` from one account to another.
-    /// This function verifies that `from` has at least `amount` balance to transfer,
-    /// and that no mathematical overflows occur.
-    pub fn transfer(&mut self, caller: String, to: String, amount: u128) -> Result<(), &'static str> {
-        let from_balance = self.balance(&caller);
-        let to_balance = self.balance(&to);
+	/// Transfer `amount` from one account to another.
+	/// This function verifies that `from` has at least `amount` balance to transfer,
+	/// and that no mathematical overflows occur.
+	pub fn transfer(
+		&mut self,
+		caller: AccountId,
+		to: AccountId,
+		amount: Balance,
+	) -> Result<(), &'static str> {
+		let from_balance = self.balance(&caller);
+		let to_balance = self.balance(&to);
 
-        from_balance.checked_sub(amount)
-            .ok_or("Not enough funds!")
-            .and_then(|new_from_balance| {
-                to_balance.checked_add(new_from_balance)
-                    .ok_or("Overflow occured!")
-                    .and_then(|new_to_balance| {
-                        self.set_balance(&caller, new_from_balance);
-                        self.set_balance(&to, new_to_balance);
-                        Ok(())
-                    })
-            })
-    }
+		from_balance
+			.checked_sub(&amount)
+			.ok_or("Not enough funds!")
+			.and_then(|new_from_balance| {
+				to_balance.checked_add(&amount).ok_or("Overflow occured!").and_then(
+					|new_to_balance| {
+						self.set_balance(caller, new_from_balance);
+						self.set_balance(to, new_to_balance);
+						Ok(())
+					},
+				)
+			})
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn init_balances() {
-        let mut balances = super::Pallet::new();
+	#[test]
+	fn init_balances() {
+		let mut balances = super::Pallet::new();
 
-        assert_eq!(balances.balance(&"Alice".to_string()), 0);
-        balances.set_balance(&"Alice".to_string(), 100);
-        assert_eq!(balances.balance(&"Alice".to_string()), 100);
-        assert_eq!(balances.balance(&"Bob".to_string()), 0);
-    }
+		assert_eq!(balances.balance(&"Alice".to_string()), 0);
+		balances.set_balance(&"Alice".to_string(), 100);
+		assert_eq!(balances.balance(&"Alice".to_string()), 100);
+		assert_eq!(balances.balance(&"Bob".to_string()), 0);
+	}
 
-    #[test]
-    fn transfer_balance() {
-        let mut balances = super::Pallet::new();
-        balances.set_balance(&"Alice".to_string(), 100);
+	#[test]
+	fn transfer_balance() {
+		let mut balances = super::Pallet::new();
+		balances.set_balance(&"Alice".to_string(), 100);
 
-        balances.transfer("Alice".to_string(), "Bob".to_string(), 50).unwrap();
-        assert_eq!(balances.balance(&"Alice".to_string()), 50);
-        assert_eq!(balances.balance(&"Bob".to_string()), 50);
+		balances.transfer("Alice".to_string(), "Bob".to_string(), 50).unwrap();
+		assert_eq!(balances.balance(&"Alice".to_string()), 50);
+		assert_eq!(balances.balance(&"Bob".to_string()), 50);
 
-        let result = balances.transfer("Alice".to_string(), "Bob".to_string(), 60);
-        assert_eq!(result, Err("Not enough funds!"));
-        assert_eq!(balances.balance(&"Alice".to_string()), 50);
-        assert_eq!(balances.balance(&"Bob".to_string()), 50);
-    }
+		let result = balances.transfer("Alice".to_string(), "Bob".to_string(), 60);
+		assert_eq!(result, Err("Not enough funds!"));
+		assert_eq!(balances.balance(&"Alice".to_string()), 50);
+		assert_eq!(balances.balance(&"Bob".to_string()), 50);
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
-
 mod balances;
 mod system;
 
+mod types {
+	pub type AccountId = String;
+	pub type Balance = u128;
+}
+
+#[derive(Debug)]
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet<crate::types::AccountId, crate::types::Balance>,
+}
+
+impl Runtime {
+	pub fn new() -> Self {
+		Runtime { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let mut runtime = Runtime::new();
+	let alice = &"alice".to_string();
+	let bob = &"bob".to_string();
+	let charlie = &"charlie".to_string();
+	runtime.balances.set_balance(alice.into(), 100);
+
+	// start emulating a block
+	runtime.system.inc_block_number();
+	assert_eq!(runtime.system.block_number, 1);
+
+	// first transaction
+	runtime.system.inc_nonce(alice);
+	let _res = runtime
+		.balances
+		.transfer(alice.into(), bob.into(), 30)
+		.map_err(|e| eprintln!("Error: {}", e));
+
+	// second transaction
+	runtime.system.inc_nonce(alice);
+	let _res = runtime
+		.balances
+		.transfer(alice.into(), charlie.into(), 20)
+		.map_err(|e| eprintln!("Error: {}", e));
+
+	// inspect the runtime state
+	println!("{:#?}", runtime);
 }
